ed5fe7c21d360678240e3d5a6657963448ee4ec9
Larissa <liulisi1990@126.com>
2022-06-22 15:17:16 +0800

    add edit/delete/add products

--- SportsStore/Models/EFStoreRepository.cs
+++ SportsStore/Models/EFStoreRepository.cs
@@ -1,6 +1,6 @@
 ﻿namespace SportsStore.Models
 {
-    public class EFStoreRepository:IStoreRepository
+    public class EFStoreRepository : IStoreRepository
     {
         private StoreDbContext context;
         public EFStoreRepository(StoreDbContext ctx)
@@ -9,5 +9,21 @@
         }
 
         public IQueryable<Product> Products => context.Products;
+        public void CreateProduct(Product p)
+        {
+            context.Add(p);
+            context.SaveChanges();
+        }
+
+        public void DeleteProduct(Product p)
+        {
+            context.Remove(p);
+            context.SaveChanges();
+        }
+
+        public void SaveProduct(Product p)
+        {
+            context.SaveChanges();
+        }
     }
 }
