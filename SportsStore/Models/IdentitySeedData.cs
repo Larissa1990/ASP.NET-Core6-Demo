9c794a99e8a76f457f28a1548ca919260bacc71b
Larissa <liulisi1990@126.com>
2022-06-23 20:15:41 +0800

    add identityuser

---
+++ SportsStore/Models/IdentitySeedData.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public static class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "Hzbp123$";
+        public static async void EnsurePopulated(IApplicationBuilder app)
+        {
+            AppIdentityDbContext context=app.ApplicationServices.CreateScope().ServiceProvider
+                .GetRequiredService<AppIdentityDbContext>();
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+            UserManager<IdentityUser> userManager = app.ApplicationServices.CreateScope().ServiceProvider
+                .GetRequiredService<UserManager<IdentityUser>>();
+            IdentityUser user= await userManager.FindByNameAsync(adminUser);
+            if (user == null)
+            {
+                user = new IdentityUser("Admin");
+                user.Email = "admin@126.com";
+                user.PhoneNumber = "8888";
+                await userManager.CreateAsync(user, adminPassword);
+            }
+        }
+    }
+}
