6085ee763b79aeaf37b38e85b4156aa67e5c46d2
Larissa <liulisi1990@126.com>
2022-06-26 17:05:38 +0800

    Add dockerfile

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -27,6 +27,18 @@ builder.Services.AddDbContext<AppIdentityDbContext>(opts => opts.UseSqlServer(
 builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
 
 var app = builder.Build();
+
+if (app.Environment.IsProduction())
+{
+    app.UseExceptionHandler("/error");
+}
+
+app.UseRequestLocalization(opts =>
+{
+    opts.AddSupportedCultures("en-US")
+    .AddSupportedUICultures("en-US")
+    .SetDefaultCulture("en-US");
+});
 app.UseStaticFiles();
 app.UseSession();
 
